dca57c270337df192493d0e170f7336aff1e7b7b
brasa <88164818+bl4z3y@users.noreply.github.com>
2024-09-08 19:38:33 -0300

    second fix l56 main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def main():
     thread.start()
 
     # Monitorar o progresso
-    while round(requests.get(progress_url).json()['progress'], 2) != (0 or 100):
+    while int(requests.get(progress_url).json()['progress']) != (0 or 100):
         os.system(CL)
         progress = round(requests.get(progress_url).json()['progress'], 2)
         print(f"Gerando imagem... ({progress * 100}%)")
